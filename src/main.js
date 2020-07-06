5f04ce69c997c4bd2cfc03eda68c418c56294b1b
valls <olavalls@gmail.com>
2020-07-05 22:01:29 -0300

    Creating js code to update dynamically progress bar width

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const post = document.querySelector('.post-wrap');
+
+let bar = document.createElement('div');
+
+bar.style.width = '0';
+bar.style.height = '5px';
+bar.style.backgroundColor = "#7159c1";
+bar.style.position = 'fixed';
+bar.style.zIndex = 9999;
+bar.style.top = 0;
+bar.style.left = 0;
+bar.style.transition = '0.2s';
+
+window.addEventListener('load', () => {
+  document.body.append(bar);
+  document.addEventListener('scroll', updateBarWidth);
+});
+
+function updateBarWidth() {
+  const postHeight = post.offsetHeight;
+  const pageHeight = window.pageYOffset;
+
+  const updatedWidth = (pageHeight * 100) / postHeight;
+  bar.style.width = updatedWidth + "%";
+}
